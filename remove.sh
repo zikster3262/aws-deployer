588c7b0dfa860e30c5348edf68afe774f3881976
zikster <zikster3262@gmail.com>
2022-01-10 19:49:36 +0100

    mygit

--- remove.sh
+++ remove.sh
@@ -21,7 +21,48 @@
       delete-internet-gateway --internet-gateway-id ${igw}
   fi
 
-  # get subnets
+# # delete launch templates
+ltmps=$(aws ec2  describe-launch-templates \
+    | jq -r .LaunchTemplates[].LaunchTemplateId)
+  if [ "${ltmps}" != "null" ]; then
+    for ltmp in ${ltmps}; do
+      echo "Deleting Launch Templates ${ltmp}"
+      aws ec2  delete-launch-template --launch-template-id ${ltmp}
+    done
+  fi
+
+
+ # delet autoscalling groups
+ ags=$(aws autoscaling describe-auto-scaling-groups|jq -r .AutoScalingGroups[0].AutoScalingGroupName)
+
+  if [ "${ags}" != "null" ]; then
+    echo "Detaching and deleting autoscalling group ${ags}"
+    aws autoscaling --auto-scaling-group-name ${ags}
+  fi
+
+#  # delete natgw
+ natgw=$(aws ec2 --region eu-central-1 \
+    describe-nat-gateways --filter Name=vpc-id,Values=${vpc} \
+    |jq .NatGateways[0].NatGatewayId)
+
+  if [ "${natgw}" != "null" ]; then
+    echo "Detaching and deleting natgw ${natgw}"
+    aws ec2 delete-nat-gateway --nat-gateway-id ${natgw}
+  fi
+
+
+#   #get routing tables
+  rt=$(aws ec2 --region eu-central-1 \
+    describe-route-tables --filter Name=vpc-id,Values=${vpc}|jq -r .RouteTables[0].RouteTableId)
+   if [ "${rt}" != "null" ]; then
+    echo "Detaching and deleting route table ${rt}"
+    aws ec2 delete-route-table --route-table-id ${rt}
+  fi
+
+
+
+
+# get subnets
   subnets=$(aws ec2 --region eu-central-1 \
     describe-subnets --filters Name=vpc-id,Values=${vpc} \
     | jq -r .Subnets[].SubnetId)
@@ -33,12 +74,8 @@
     done
   fi
 
-  # delete default vpc
+
+#   # delete default vpc
   echo "Deleting vpc ${vpc}"
   aws ec2 --region eu-central-1 \
     delete-vpc --vpc-id ${vpc}
-
-
-# mongosh "mongodb+srv://zikster:Tran3262@app.hykmu.mongodb.net/myFirstDatabase"
-# use AWS
-# db.vpcs.remove({})
